a7a734e3ad52c5444499f4782fc02d495b200c1f
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-09 02:24:16 +0530

    Updated config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CToFiQQR#NVfb5ixTd4J-_SeRxRhrW2nJcQQAq1cO4qTqJ1jmkMo",
+SESSION_ID: process.env.SESSION_ID || "WbBB2bRK#aiumGjXHqmSfOO03G5fWcHu1ZZKFGA645iuz30mxq-0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/chathunga2007/Chathunga-Dev-WhatsApp-Bot/blob/main/images/Chathunga-Dev-WhatsApp-Bot-Image.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Chathunga-Dev Chat Bot Is Live Now😍*",
 BOT_OWNER: '94767945968',
