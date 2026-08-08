0532ced77ea104a77c13520829421b65d20bbd6b
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-08 23:34:27 +0530

    Session ID Added!

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "DDRXXLzK#_aTe5gg0oXCszvxFaqS4XfKcxxkz__K2yeLrKSeFR6s",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/chathunga2007/Chathunga-Dev-WhatsApp-Bot/blob/main/images/Chathunga-Dev-WhatsApp-Bot-Image.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Chathunga-Dev Is Alive Now😍*",
 BOT_OWNER: '94767945968',
