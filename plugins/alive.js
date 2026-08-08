ae0a435fdf4d2fdc400fdf77e1a9d01656fd3a1c
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-08-09 00:16:49 +0530

    Updated alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -2,7 +2,7 @@ const { cmd, commands } = require('../command');
 const config = require('../config');
 
 cmd({
-    pattern: "alive",
+    pattern: "hi"+"HI",
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
